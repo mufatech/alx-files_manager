ff5f460cbaa209fbbcbe3b6e4e8f9129a68f7662
mufatech <fabosedesidiqat@gmail.com>
2023-12-19 15:53:25 +0100

    Files Manager

---
+++ tests/0-redisAndMongo.test.js
@@ -0,0 +1,23 @@
+import { expect, use, should } from 'chai';
+import chaiHttp from 'chai-http';
+import { promisify } from 'util';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+use(chaiHttp);
+should();
+
+// redisClient
+
+describe('testing the clients for MongoDB and Redis', () => {
+  describe('redis Client', () => {
+    before(async () => {
+      await redisClient.client.flushall('ASYNC');
+    });
+
+    after(async () => {
+      await redisClient.client.flushall('ASYNC');
+    });
+
+    it('shows that connection is alive', async () => {
+      expect(redisClient.isAlive()).to.equal(true);
